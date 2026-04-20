e7bfc9668fa8947a74d8ce163181ed2e83a3a643
pypycodes <pypycodess@gmail.com>
2026-04-20 14:50:55 +0530

    chore: package.json

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.suraksha',
+  appName: 'Suraksha',
+  webDir: 'dist'
+};
+
+export default config;
